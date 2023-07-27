a1c74c7face1e44ffeedca586eadd7a9771b501d
balajik27 <balajik272003@gmail.com>
2023-07-27 20:53:44 +0530

    multi

--- C2TC/src/OOPs/MultiLevel.java
+++ C2TC/src/OOPs/MultiLevel.java
@@ -1,17 +1,17 @@
 package OOPs;
 
 
-public class Parent{
+class Parent{
 	void display1() {
 		System.out.println("This is parent");
 	}
 }
-public class child extends Parent{
+class child extends Parent{
 	void display2() {
 		System.out.println("This is child");
 	}
 }
-public class MultiLevel extends child{
+class MultiLevel extends child{
 	void display3() {
 		System.out.println("This is third child");
 	}
