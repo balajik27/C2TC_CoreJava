2c7b50db296c2e555716ce68b7b13d793813316f
balajik27 <balajik272003@gmail.com>
2023-07-28 16:05:53 +0530

    final commit

---
+++ C2TC/src/com/cg/daysix/FinalEg/FinalClass.java
@@ -0,0 +1,10 @@
+package com.cg.daysix.FinalEg;
+
+final public class FinalClass {
+	int a = 10;
+}
+
+public class child extends FinalClass{
+	int b=30;
+	
+}
--- C2TC/src/com/cg/daysix/FinalEg/Main.java
+++ C2TC/src/com/cg/daysix/FinalEg/Main.java
@@ -1,5 +1,11 @@
 package com.cg.daysix.FinalEg;
 
 public class Main {
-
+	public static void main(String a[]) {
+		FinalClass f = new FinalClass();
+		child c = new child(); //The type child cannot subclass the final class FinalClass
+		System.out.println(c.a); // error in before line
+		System.out.println(f.a); 
+		
+	}
 }
