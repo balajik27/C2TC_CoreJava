6e6a25e37a78e0fa44fe57d55be0e7c829f0ea5e
balajik27 <balajik272003@gmail.com>
2023-07-28 13:02:37 +0530

    day six

---
+++ C2TC/src/com/cg/daysix/StaticEg/Main.java
@@ -0,0 +1,9 @@
+package com.cg.daysix.StaticEg;
+
+public class Main {
+	public static void main(String a[]) {
+		StaticVariable sv = new StaticVariable();
+		sv.display();
+		
+	}
+}
---
+++ C2TC/src/com/cg/daysix/StaticEg/StaticVariable.java
@@ -0,0 +1,14 @@
+package com.cg.daysix.StaticEg;
+
+public class StaticVariable {
+	static int a = 10;
+	static {
+		a = 20;
+	}
+	void display()
+	{
+		System.out.println(a);
+	}
+//	System.out.println(a);
+	
+}
