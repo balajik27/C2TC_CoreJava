0a8f69424539847b4eed85af7e95528de014c08d
balajik27 <balajik272003@gmail.com>
2023-07-28 16:18:31 +0530

    final commit

--- C2TC/src/com/cg/daysix/FinalEg/FinalClass.java
+++ C2TC/src/com/cg/daysix/FinalEg/FinalClass.java
@@ -1,11 +1,21 @@
 package com.cg.daysix.FinalEg;
 
-final public class FinalClass {
+public class FinalClass {
 	int a = 10;
 	final int var = 20;
 //	var = 30; error because final variable
+	
+	final public void display() {
+		System.out.println("cant override");
+	}
 }
 
+class FinalMethod extends FinalClass{
+//	@Override
+//	public void display() { //  cant be overriden
+//		System.out.println("Cant override");
+//	}
+}
 public class child extends FinalClass{
 	int b=30;
 	
