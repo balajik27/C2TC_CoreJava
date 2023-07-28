dbc5f87b8b49b656b297ccc0bee3e50b411b3e79
balajik27 <balajik272003@gmail.com>
2023-07-28 16:08:52 +0530

    final variable

--- C2TC/src/com/cg/daysix/FinalEg/FinalClass.java
+++ C2TC/src/com/cg/daysix/FinalEg/FinalClass.java
@@ -2,6 +2,8 @@ package com.cg.daysix.FinalEg;
 
 final public class FinalClass {
 	int a = 10;
+	final int var = 20;
+//	var = 30; error because final variable
 }
 
 public class child extends FinalClass{
